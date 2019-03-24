f01eeb3df7cd0dcbe4a98aea0edcc80a8de313c9
Matthias Busenhart <busenham@student.ethz.ch>
2019-03-24 10:38:45 +0100

    Improve Spectral Drawing

--- test/io/WriteEdges.py
+++ test/io/WriteEdges.py
@@ -4,18 +4,9 @@ edges = 200
 nodes = 100
 
 with open("edges.txt","w") as wr:
-    wr.write("#include \"../../src/structures/MatrixGraph.hpp\"\n")
-    wr.write("#include \"../../src/io/WriteTikz.hpp\"\n")
-    wr.write("#include <iostream>\n")
-
-    wr.write("int main(int argc, char const *argv[]) {\n")
-    wr.write("  using mgraph = gl::MGraph<double>;\n")
-    wr.write("  mgraph g(%s);\n" % nodes)
     for i in range(edges):
-        x = randint(0, nodes-1)
-        y = randint(0, nodes-1)
-        wr.write("  g.setEdge(%s, %s);\n" % (x,y))
-        wr.write("  g.setEdge(%s, %s);\n" % (x,y))
-    wr.write("  gl::writeTikzToStream2(std::cout, g);\n")
-    wr.write("  return 0;\n")
-    wr.write("}\n")
+        p1 = randint(0, nodes-1)
+        p2 = randint(0, nodes-1)
+        wr.write("%s %s 1\n" % (p1, p2))
+        wr.write("%s %s 1\n" % (p2, p1)) # undirected graph
+    
