e1f6283c4eee556cb76f948582bfaf792d813a28
Matthias Busenhart <busenham@student.ethz.ch>
2019-03-25 12:55:15 +0100

    Update to sparse Update to sparse

--- src/io/WriteImage.hpp
+++ src/io/WriteImage.hpp
@@ -3,7 +3,9 @@
 
 #include "../structures/Graph.hpp"
 #include <iostream>
+#include <Eigen/Sparse>
 #include <Eigen/Dense>
+#include <vector>
 #include <mgl2/mgl.h>
 
 namespace gl
@@ -18,20 +20,25 @@ namespace gl
 template <class SCALAR>
 void saveImage(mglGraph* gr, gl::Graph<SCALAR> &g, const char* lineArgs = "B")
 {
-    using E_MAT = Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic>;
-    Eigen::Matrix<double, Eigen::Dynamic, 1> degs(g.numNodes());
-    E_MAT adjMat(g.numNodes(), g.numNodes());
-    E_MAT degMat;
+    //using E_MAT = Eigen::SparseMatrix;
 
-    degMat.setZero();
-    adjMat.setZero();
+    std::vector<Eigen::Triplet<double> > triplets;
+    Eigen::Matrix<double, Eigen::Dynamic, 1> degs(g.numNodes());
+    //E_MAT adjMat(g.numNodes(), g.numNodes());
+    //E_MAT degMat;
+
+    //degMat.setZero();
+    //adjMat.setZero();
 
     // build degree matrix
     for (typename Graph<SCALAR>::idx_t i = 0; i < g.numNodes(); i++)
     {
-        degs(i) = g.getDegree(i);
+        //degs(i) = g.getDegree(i);
+        triplets.emplace_back(i, i, g.getDegree(i));
     }
-    degMat = degs.asDiagonal();
+    //degMat = degs.asDiagonal();
+
+    std::cout << "Degree matrix built" << std::endl;
 
     // build adjacency matrix
     for (typename Graph<SCALAR>::idx_t i = 0; i < g.numNodes(); i++)
@@ -39,30 +46,45 @@ void saveImage(mglGraph* gr, gl::Graph<SCALAR> &g, const char* lineArgs = "B")
         typename Graph<SCALAR>::idx_list_t neighbours = g.getNeighbours(i);
         for (auto n : neighbours)
         {
-            adjMat(i, n) = 1;
-            adjMat(n, i) = 1; // undirected
+            //adjMat(i, n) = 1;
+            //adjMat(n, i) = 1; // undirected
+            triplets.emplace_back(i, n, -1);
+            triplets.emplace_back(n, i, -1);
         }
     }
 
-    E_MAT laplacian = degMat - adjMat;
+    std::cout << "Adjacency matrix built" << std::endl;
+
+    //E_MAT laplacian = degMat - adjMat;
+    Eigen::SparseMatrix<double> laplacian(g.numNodes(), g.numNodes());
+    laplacian.setFromTriplets(triplets.begin(), triplets.end());
+    laplacian.makeCompressed();
+
+    std::cout << "Laplacian matrix built" << std::endl;
 
     // calculate eigen vectors
-    Eigen::EigenSolver<E_MAT> solver;
+    Eigen::SelfAdjointEigenSolver<Eigen::SparseMatrix<double>> solver;
     solver.compute(laplacian);
+
+    std::cout << "Eigenvectors computed" << std::endl;
 
     Eigen::MatrixXd positions(2, g.numNodes());
 
     for (typename Graph<SCALAR>::idx_t i = 0; i < g.numNodes(); i++)
     {
         Eigen::Vector2d pos;
-        pos << solver.eigenvectors()(0, i).real(), solver.eigenvectors()(1, i).real();
+        auto current = solver.eigenvectors();
+        pos << current(0, i), current(1, i);
 
         // Scale by degree
         pos.normalize();
-        pos *= (1 + g.getDegree(i));
+        pos *= 15;
+        pos /= (1 + g.getDegree(i)); // Todo: nodes with high degree are now outside, they should be in the middle
 
         positions.col(i) = pos;
     }
+
+    std::cout << "Positions calculated" << std::endl;
 
     // set plot ranges
     gr->SetRange('x', positions.row(0).minCoeff(), positions.row(0).maxCoeff());
