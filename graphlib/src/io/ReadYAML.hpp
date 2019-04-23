f5eec9a49008b62fce4048a66085b8586f7e62f8
Matthias Busenhart <busenham@student.ethz.ch>
2019-04-23 21:08:54 +0200

    Add comments

--- graphlib/src/io/ReadYAML.hpp
+++ graphlib/src/io/ReadYAML.hpp
@@ -8,172 +8,206 @@
 
 namespace gl
 {
-namespace io
+namespace external
 {
-// define those macros for usage with printing
-/* usage: get the IO_Graph_variant using YAMLReader::get
- * then, write your normal code lines using IO_CALL_ON_GRAPH and the arugment IO_GRAPH
- * e.g. for pringing (and graph_variant_type g = reader.get();)
- * IO_CALL_ON_GRAPH(g, std::cout << IO_GRAPH << std::endl;);
- * 
- * If it must be called directly, use (IO_GRAPH).FUNCTION, e.g
- * (IO_GRAPH).setEdge(0, 1);
+
+/**
+ * @brief Template object to use with IO_CALL_ON_GRAPH
  */
-#define IO_GRAPH *arg
+#define IO_GRAPH (*arg)
+
+/**
+ * @brief Custom function on parsed graphs
+ * @param g The graph (graph_variant_type) got from YAMLReader::get
+ * @param func A normal line of c++ to be executed (use IO_GRAPH to template the graph object)
+ */
 #define IO_CALL_ON_GRAPH(g, func) std::visit([&](auto arg) { func; }, g)
 
+/**
+ * @brief Class to Read from a YAML file
+ */
 class YAMLReader
 {
 public:
+    /**
+     * @brief All possible variants of graphs
+     */
     using graph_variant_type = std::variant<graphMdu *, graphMdd *, graphLdu *, graphLdd *,
                                             graphMfu *, graphMfd *, graphLfu *, graphLfd *,
                                             graphMiu *, graphMid *, graphLiu *, graphLid *>;
+    /**
+     * @brief Default constructor
+     */
+    YAMLReader() {}
+    /**
+     * @brief Construct with filename
+     */
+    YAMLReader(const char *filename);
 
-    graph_variant_type graph;
+    /**
+     * @brief Set the Filename to use
+     * @param filename Location of file to read from
+     */
+    void setFilename(const char *filename);
 
-    graph_variant_type get() { return graph; }
+    /**
+     * @brief Get the graph-variant
+     * @return A variable containing a pointer to a graph variant
+     */
+    graph_variant_type get() { return graph_; }
 
-    YAMLReader()
+private:
+    /**
+     * @brief Reads the file, generates graph
+     */
+    void read();
+    std::fstream stream_;      ///< the filestream to use
+    graph_variant_type graph_; ///< the graph variant
+};
+
+YAMLReader::YAMLReader(const char *filename)
+{
+    std::cout << "Reading " << filename << std::endl;
+    stream_ = std::fstream(filename);
+    graph_ = graph_variant_type(); // set to default
+    GL_ASSERT(stream_.is_open(), "File could not be opened");
+}
+
+void YAMLReader::setFilename(const char *filename)
+{
+    std::cout << "Reading " << filename << std::endl;
+    stream_ = std::fstream(filename);
+    GL_ASSERT(stream_.is_open(), "File could not be opened");
+}
+
+void YAMLReader::read()
+{
+    // temp variables to store data while reading
+    std::string value_type, storage_type, direction_type, graph_label;
+    int number_of_nodes;
+
+    // {{from,to},{weight, color}}
+    using edge_t = std::pair<std::pair<unsigned int, unsigned int>, std::pair<double, gl::Color>>;
+    using idx_t = graphMdd::idx_t; // use idx_t from a graph
+
+    std::vector<edge_t> edges;
+    std::string line;
+    while (getline(stream_, line))
     {
-    }
-    YAMLReader(const char *filename)
-    {
-        std::cout << "Reading " << filename << std::endl;
-        stream_ = std::fstream(filename);
-        graph = graph_variant_type();
-        GL_ASSERT(stream_.is_open(), "File could not be opened");
-    }
-
-    void read()
-    {
-        std::string value_type, storage_type, direction_type, graph_label;
-        int number_of_nodes;
-
-        // {{from,to},{weight, color}}
-        using edge_t = std::pair<std::pair<unsigned int, unsigned int>, std::pair<double, gl::Color>>;
-        using idx_t = graphMdd::idx_t;
-
-        std::vector<edge_t> edges;
-        std::string line;
-        while (getline(stream_, line))
+        // comments on lines
+        if (line[0] == '#' || line.empty())
+            continue;
+        std::size_t delimiterPos = line.find(": "); // including space
+        std::string name = line.substr(0, delimiterPos);
+        std::string value = line.substr(delimiterPos + 2);
+        if (name == "value_type")
         {
-            // comments on lines
-            if (line[0] == '#' || line.empty())
-                continue;
-            std::size_t delimiterPos = line.find(": "); // including space
-            std::string name = line.substr(0, delimiterPos);
-            std::string value = line.substr(delimiterPos + 2);
-            if (name == "value_type")
+            value_type = value;
+        }
+        else if (name == "direction_type")
+        {
+            direction_type = value;
+        }
+        else if (name == "storage_type")
+        {
+            storage_type = value;
+        }
+        else if (name == "number_nodes")
+        {
+            number_of_nodes = std::stoi(value);
+        }
+        else if (name == "name")
+        {
+            graph_label = value;
+        }
+        else if (name == "edge")
+        {
+            std::stringstream ss(value);
+            idx_t a, b;
+            double weight = 1;
+            ss >> a >> b;
+            // if ss not empty, read weight
+            if (!ss.eof())
             {
-                value_type = value;
+                ss >> weight;
             }
-            else if (name == "direction_type")
+            gl::Color c(0, 0, 0);
+            unsigned int hex;
+            if (!ss.eof())
             {
-                direction_type = value;
+                ss >> std::hex >> hex;
+                c.hex(hex);
             }
-            else if (name == "storage_type")
-            {
-                storage_type = value;
-            }
-            else if (name == "number_nodes")
-            {
-                number_of_nodes = std::stoi(value);
-            }
-            else if (name == "name")
-            {
-                graph_label = value;
-            }
-            else if (name == "edge")
-            {
-                std::stringstream ss(value);
-                idx_t a, b;
-                double weight = 1;
-                ss >> a >> b;
-                // if ss not empty, read weight
-                if (!ss.eof())
-                {
-                    ss >> weight;
-                }
-                gl::Color c(0, 0, 0);
-                unsigned int hex;
-                if (!ss.eof())
-                {
-                    ss >> std::hex >> hex;
-                    c.hex(hex);
-                }
-                edges.push_back({{a, b}, {weight, c}});
-            }
-            else
-            {
-                std::cout << "Unrecognized option" << std::endl;
-            }
-        }
-
-        if (value_type == "double" && storage_type == "Matrix" && direction_type == "Undirected")
-        {
-            graph = new graphMdu(number_of_nodes, graph_label);
-        }
-        else if (value_type == "double" && storage_type == "Matrix" && direction_type == "Directed")
-        {
-            graph = new graphMdd(number_of_nodes, graph_label);
-        }
-        else if (value_type == "double" && storage_type == "List" && direction_type == "Undirected")
-        {
-            graph = new graphLdu(number_of_nodes, graph_label);
-        }
-        else if (value_type == "double" && storage_type == "List" && direction_type == "Directed")
-        {
-            graph = new graphLdd(number_of_nodes, graph_label);
-        }
-        else if (value_type == "float" && storage_type == "Matrix" && direction_type == "Undirected")
-        {
-            graph = new graphMfu(number_of_nodes, graph_label);
-        }
-        else if (value_type == "float" && storage_type == "Matrix" && direction_type == "Directed")
-        {
-            graph = new graphMfd(number_of_nodes, graph_label);
-        }
-        else if (value_type == "float" && storage_type == "List" && direction_type == "Undirected")
-        {
-            graph = new graphLfu(number_of_nodes, graph_label);
-        }
-        else if (value_type == "float" && storage_type == "List" && direction_type == "Directed")
-        {
-            graph = new graphLfd(number_of_nodes, graph_label);
-        }
-        else if (value_type == "int" && storage_type == "Matrix" && direction_type == "Undirected")
-        {
-            graph = new graphMiu(number_of_nodes, graph_label);
-        }
-        else if (value_type == "int" && storage_type == "Matrix" && direction_type == "Directed")
-        {
-            graph = new graphMid(number_of_nodes, graph_label);
-        }
-        else if (value_type == "int" && storage_type == "List" && direction_type == "Undirected")
-        {
-            graph = new graphLiu(number_of_nodes, graph_label);
-        }
-        else if (value_type == "int" && storage_type == "List" && direction_type == "Directed")
-        {
-            graph = new graphLid(number_of_nodes, graph_label);
+            edges.push_back({{a, b}, {weight, c}});
         }
         else
         {
-            GL_ASSERT(false, "Please enter valid data into config.");
-        }
-
-        // add all edges to the graph
-        for (edge_t &e : edges)
-        {
-            IO_CALL_ON_GRAPH(graph, (IO_GRAPH).setEdge(e.first.first, e.first.second, e.second.first, e.second.second));
+            std::cout << "Unrecognized option" << std::endl;
         }
     }
 
-private:
-    std::fstream stream_;
-};
+    // switch graph_ on current state from reading
+    if (value_type == "double" && storage_type == "Matrix" && direction_type == "Undirected")
+    {
+        graph_ = new graphMdu(number_of_nodes, graph_label);
+    }
+    else if (value_type == "double" && storage_type == "Matrix" && direction_type == "Directed")
+    {
+        graph_ = new graphMdd(number_of_nodes, graph_label);
+    }
+    else if (value_type == "double" && storage_type == "List" && direction_type == "Undirected")
+    {
+        graph_ = new graphLdu(number_of_nodes, graph_label);
+    }
+    else if (value_type == "double" && storage_type == "List" && direction_type == "Directed")
+    {
+        graph_ = new graphLdd(number_of_nodes, graph_label);
+    }
+    else if (value_type == "float" && storage_type == "Matrix" && direction_type == "Undirected")
+    {
+        graph_ = new graphMfu(number_of_nodes, graph_label);
+    }
+    else if (value_type == "float" && storage_type == "Matrix" && direction_type == "Directed")
+    {
+        graph_ = new graphMfd(number_of_nodes, graph_label);
+    }
+    else if (value_type == "float" && storage_type == "List" && direction_type == "Undirected")
+    {
+        graph_ = new graphLfu(number_of_nodes, graph_label);
+    }
+    else if (value_type == "float" && storage_type == "List" && direction_type == "Directed")
+    {
+        graph_ = new graphLfd(number_of_nodes, graph_label);
+    }
+    else if (value_type == "int" && storage_type == "Matrix" && direction_type == "Undirected")
+    {
+        graph_ = new graphMiu(number_of_nodes, graph_label);
+    }
+    else if (value_type == "int" && storage_type == "Matrix" && direction_type == "Directed")
+    {
+        graph_ = new graphMid(number_of_nodes, graph_label);
+    }
+    else if (value_type == "int" && storage_type == "List" && direction_type == "Undirected")
+    {
+        graph_ = new graphLiu(number_of_nodes, graph_label);
+    }
+    else if (value_type == "int" && storage_type == "List" && direction_type == "Directed")
+    {
+        graph_ = new graphLid(number_of_nodes, graph_label);
+    }
+    else
+    {
+        GL_ASSERT(false, "Please enter valid data into config.");
+    }
 
-} // namespace io
+    // add all edges to the graph
+    for (edge_t &e : edges)
+    {
+        IO_CALL_ON_GRAPH(graph_, (IO_GRAPH).setEdge(e.first.first, e.first.second, e.second.first, e.second.second));
+    }
+}
+
+} // namespace external
 } // namespace gl
 
 #endif
--- test/Test_ReadYAML.cpp
+++ test/Test_ReadYAML.cpp
@@ -3,21 +3,18 @@
 
 int main(int argc, char const *argv[])
 {
-    gl::io::YAMLReader reader("test/graph.yml");
+    gl::external::YAMLReader reader("test/graph.yml");
 
-    reader.read();
+    gl::external::YAMLReader::graph_variant_type g = reader.get();
 
-    gl::io::YAMLReader::graph_variant_type g = reader.get();
-
-    // auto print_graph = [](auto g_ptr) { std::cout << *g_ptr; };
-    // std::visit(print_graph, g);
+    // std::visit([](auto g_ptr) { std::cout << *g_ptr; }, g);
     IO_CALL_ON_GRAPH(g, std::cout << IO_GRAPH << std::endl);
 
     std::ofstream out("build/test/OUTPUT.tex");
 
     //std::visit([&out](auto arg) { gl::external::writeTikzToStream2(out, *arg); }, g);
     IO_CALL_ON_GRAPH(g, gl::external::writeTikzToStream2(out, IO_GRAPH));
-
+    IO_CALL_ON_GRAPH(g, IO_GRAPH.setEdge(1, 0, 3.4));
     out.close();
 
     return 0;
