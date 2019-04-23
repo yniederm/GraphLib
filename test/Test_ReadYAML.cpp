0d5dbd3640e6bfc4c3b339f0f4589d6928e1d443
Matthias Busenhart <busenham@student.ethz.ch>
2019-04-23 16:52:09 +0200

    Fix not needed var

--- test/Test_ReadYAML.cpp
+++ test/Test_ReadYAML.cpp
@@ -2,14 +2,15 @@
 
 int main(int argc, char const *argv[])
 {
-    const int idx = 0;
     gl::io::YAMLReader reader("../test/graph.yml");
 
     reader.read();
 
     gl::io::YAMLReader::graph_variant_type g = reader.get();
 
-    auto add_edge = [](auto g_ptr) { g_ptr->setEdge(0, 1); };
+    auto add_edge = [](auto g_ptr) { 
+        g_ptr->setEdge(0, 1);
+        g_ptr->setEdge(1, 2, 3); };
     auto print_graph = [](auto g_ptr) { std::cout << *g_ptr; };
 
     std::visit(add_edge, g);
